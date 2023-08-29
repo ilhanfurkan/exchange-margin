e81823b6d72a54a0f262f04023ff7e337c46f981
AhmetMel1 <128613537+AhmetMel1@users.noreply.github.com>
2023-08-29 14:19:35 +0300

    New Branch

--- services/okx.js
+++ services/okx.js
@@ -1,30 +1,34 @@
 // const { response } = require('express');
-const okxRequest = require('../helper/okx_request');
-const response = require('../helper/response');
-const axios = require('axios');
+const okxRequest = require("../helper/okx_request");
+const response = require("../helper/response");
+const axios = require("axios");
 
-exports.getInstruments = async function() {
-    try {
-        const method = "GET";
-      
-        const newHead = await okxRequest.newGetRequestHeaders(
-            process.env.OKX_API_KEY,
-            process.env.OKX_PASSPHRASE,
-            process.env.OKX_SECRET_KEY,
-            process.env.OKX_BASE_PATH+'public/instruments?instType=SPOT'
-        );
-           console.log(process.env.OKX_URL+process.env.OKX_BASE_PATH+'public/instruments?instType=SPOT')
-        const result = await axios.get(
-            process.env.OKX_URL+process.env.OKX_BASE_PATH+'public/instruments?instType=SPOT',
-            {
-                headers: {
-                    ...newHead
-                },
-            }
-        );
-        console.log('result',result)
-        return result.data.data;
-    } catch (error) {
-        return response.errorResponse(res, "Bad Request", 400, error.message)
-    }
-}
+exports.getInstruments = async function () {
+  try {
+    const newHead = await okxRequest.newGetRequestHeaders(
+      process.env.OKX_API_KEY,
+      process.env.OKX_PASSPHRASE,
+      process.env.OKX_SECRET_KEY,
+      process.env.OKX_BASE_PATH + "public/instruments?instType=SPOT"
+    );
+    console.log(
+      process.env.OKX_URL +
+        process.env.OKX_BASE_PATH +
+        "public/instruments?instType=SPOT"
+    );
+    const result = await axios.get(
+      process.env.OKX_URL +
+        process.env.OKX_BASE_PATH +
+        "public/instruments?instType=SPOT",
+      {
+        headers: {
+          ...newHead,
+        },
+      }
+    );
+    console.log("result", result);
+    return result.data.data;
+  } catch (error) {
+    return response.errorResponse(res, "Bad Request", 400, error.message);
+  }
+};
