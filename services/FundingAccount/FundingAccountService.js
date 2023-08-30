7d9163dbf75b98e4ae80900e52ff76ecf8700e8a
AhmetMel1 <128613537+AhmetMel1@users.noreply.github.com>
2023-08-30 15:48:11 +0300

    Example added.

--- services/FundingAccount/FundingAccountService.js
+++ services/FundingAccount/FundingAccountService.js
@@ -3,11 +3,24 @@ const {
   getBalancesRequest,
   getBalancesResponse,
 } = require("../../utils/OKX/FundingAccount/GetBalances");
+const {
+  postAssetTransferRequest,
+  postAssetTransferResponse,
+} = require("../../utils/OKX/FundingAccount/PostAssetTransfer");
 exports.getBalances = async (body) => {
   return newGetRequest(
     getBalancesRequest,
     getBalancesResponse,
     "asset/balances",
+    body
+  );
+};
+
+exports.postAssetTransfer = async (body) => {
+  return newPostRequest(
+    postAssetTransferRequest,
+    postAssetTransferResponse,
+    "asset/transfer",
     body
   );
 };
