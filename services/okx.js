122b57f57cdcf1e03e32b7a792cc90965b33ee19
AhmetMel1 <128613537+AhmetMel1@users.noreply.github.com>
2023-08-30 14:43:45 +0300

    Bug fix.

--- services/okx.js
+++ services/okx.js
@@ -11,18 +11,18 @@ const { makeResponse } = require("../middleware/okxResponse");
 
 exports.getInstruments = async function (body) {
   try {
-    const params = okxRequest.newQueryParams(body);
+    const query = okxRequest.newQueryParams(body);
     const newHead = await okxRequest.newGetRequestHeaders(
       "328f1b3a-ade9-47e1-a90e-48c534884bc8",
       "G67O!b#AbOsT530C3e8FBg6g",
       "6E9D31BA87D0265F444D68E9B7C3DEB1",
-      process.env.OKX_BASE_PATH + "trade/orders-history-archive" + params
+      process.env.OKX_BASE_PATH + "trade/orders-history-archive" + query
     );
     const result = await axios.get(
       process.env.OKX_URL +
         process.env.OKX_BASE_PATH +
         "trade/orders-history-archive" +
-        params,
+        query,
       {
         headers: {
           ...newHead,
