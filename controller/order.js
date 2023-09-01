b3098ffbc8637c64a56fa5c482a4a6178c36f304
AhmetMel1 <128613537+AhmetMel1@users.noreply.github.com>
2023-09-01 10:37:34 +0300

    Bug fix.

--- controller/order.js
+++ controller/order.js
@@ -1,24 +1,26 @@
-
 const {
   fillOrderHistoryArchive,
   fillOrderRequest,
 } = require("../services/Order/OrderService");
 
-const {defaultResponse, errorResponse} = require("../helpers/response");
-const {getOrderHistoryArchive} = require("../services/OKX/OrderBookTrading/OrderBookTradingService");
+const {
+  getOrderHistoryArchive,
+} = require("../services/OKX/OrderBookTrading/OrderBookTradingService");
 const {
   successResponse,
   errorResponse,
   defaultResponse,
 } = require("../helpers/response");
 
-const { postPlaceOrder } = require("../services/OKX/OrderBookTrading/OrderBookTradingService");
+const {
+  postPlaceOrder,
+} = require("../services/OKX/OrderBookTrading/OrderBookTradingService");
 const { placeOrders } = require("../services/Order/OrderService");
 
 exports.postPlaceOrder = async (req, res) => {
   try {
     const request = await postPlaceOrder(req.body);
-    await placeOrders(request,res);
+    await placeOrders(request, res);
     defaultResponse(res, request, 200, "OK: Successful");
   } catch (error) {
     errorResponse("Bad Request", 400);
@@ -27,12 +29,11 @@ exports.postPlaceOrder = async (req, res) => {
 
 exports.getOrderHistory = async (req, res) => {
   try {
-    const orderHistoryList = await getOrderHistoryArchive({instType:"SPOT"});
-  
+    const orderHistoryList = await getOrderHistoryArchive({ instType: "SPOT" });
+
     const orderHistory = await fillOrderHistoryArchive(orderHistoryList);
-    
+
     defaultResponse(res, orderHistoryList, 200, "OK: Successful");
-    
   } catch (error) {
     errorResponse("Bad Request", 400);
   }
