dbdcdd4de167d89aca133a008f3b4bffdb230d8b
AhmetMel1 <128613537+AhmetMel1@users.noreply.github.com>
2023-09-01 10:42:35 +0300

    Bug fix.

--- services/Order/OrderService.js
+++ services/Order/OrderService.js
@@ -3,21 +3,21 @@ const { fillMarketRequest } = require("../../utils/Market/FillMarketRequest");
 const marginOrderHistoryDocument = require("../../models/marginOrderHistoryDocument");
 
 const MarginOrderHistoryArchiveDocument = require("../../models/MarginOrderHistoryArchiveDocument");
-const {fillOrderHistoryArchive} = require("../../utils/Order/OrderHistory");
+const { fillOrderHistoryArchive } = require("../../utils/Order/OrderHistory");
 const marginOrderRequestDocument = require("../../models/marginOrderRequestDocument");
 const { OrderRequest } = require("../../utils/Order/OrderRequest");
 
-exports.placeOrders = async (request,response) => {
-  console.log(request,"slam")
+exports.placeOrders = async (request, response) => {
+  console.log(request, "slam");
   if (request?.status === "0") {
-    await new marginOrderRequestDocument(OrderRequest(request,response)).save();
+    await new marginOrderRequestDocument(
+      OrderRequest(request, response)
+    ).save();
     return await marginOrderRequestDocument.find({}).exec();
+  } else {
+    console.log(item);
+    return await item.message;
   }
-  else{
-  console.log(item)
-    return await item.message
-  }
- 
 };
 
 exports.fillOrderRequest = async (items) => {
@@ -34,11 +34,16 @@ exports.fillOrderRequest = async (items) => {
 
 exports.fillOrderHistoryArchive = async (items) => {
   items.forEach(async (item) => {
-    // const coinCheck = await MarginOrderHistoryArchiveDocument.find({
-    //   _id: item.ordId,
-      await new MarginOrderHistoryArchiveDocument(fillOrderHistoryArchive(item)).save();
+    const historyCheck = await MarginOrderHistoryArchiveDocument.find({
+      _id: item.ordId,
     });
- 
+    if (!historyCheck) {
+      await new MarginOrderHistoryArchiveDocument(
+        fillOrderHistoryArchive(item)
+      ).save();
+    }
+  });
+
   return await MarginOrderHistoryArchiveDocument.find({}).exec();
 };
 exports.fillOrderHistory = async (items) => {
